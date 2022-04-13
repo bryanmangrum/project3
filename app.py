132482c399dafe4d3e083af201f45adaa6a9b007
charlesphil <71610407+charlesphil@users.noreply.github.com>
2022-04-12 21:33:33 -0400

    Created player salaries treemap

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-# Initial connection to sqlite database
+# Initial connection to database
 engine = create_engine("postgresql://postgres:password@localhost:5432"
                        "/basketball")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-
-# For debugging to see if our tables are in our Base
-# print(Base.classes.keys())
 
 # Reflect tables
 Player_Salaries = Base.classes.player_2020_salaries
@@ -30,6 +27,51 @@ def index():
     session.close()
 
     return render_template("index.html")
+
+
+@app.route("/salaries")
+def test():
+    # Initialize database session
+    session = Session(engine)
+
+    # Base labels and parents
+    labels = ["League", "Western Conference", "Eastern Conference"]
+    parents = ["", "League", "League"]
+
+    # Query for distinct divisions and their parent conferences
+    results = session.query(Win_Counts.division, Win_Counts.conference)\
+        .distinct()
+
+    for row in results:
+        # append distinct divisions to labels
+        labels.append(row[0])
+        # append their parent conferences to parents
+        parents.append(row[1])
+
+    # Query distinct teams
+    team_results = session.query(Win_Counts.team, Win_Counts.division)\
+        .distinct()
+
+    for row in team_results:
+        # append distinct teams to labels
+        labels.append(row[0])
+        # append their parent divisions to parents
+        parents.append(row[1])
+        # new query using current row's team name
+        player_results = session.query(
+            Player_Salaries.player,
+            Player_Salaries.team
+        ).filter(Player_Salaries.team == row[0])
+        for player in player_results:
+            # append distinct players into labels
+            labels.append(player[0])
+            # append parent team into parents
+            parents.append(player[1])
+
+    # Close connection
+    session.close()
+
+    return render_template("salaries.html", labels=labels, parents=parents)
 
 
 if __name__ == "__main__":
