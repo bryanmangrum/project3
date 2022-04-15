dd65f4f15d5df3c67c0b6bcbefb98826fc36f7cd
charlesphil <71610407+charlesphil@users.noreply.github.com>
2022-04-14 23:28:05 -0400

    Added NBA team color table

--- app.py
+++ app.py
@@ -23,10 +23,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     session = Session(engine)
-    results = session.query(Team_Attributes.arena).all()
+    arenaInfo = []
+    result = session.query(Arenas.latitude, Arenas.longitude, Arenas.arena,
+                           Arenas.team, Arenas.arenaurl, Arenas.sponsorurl)
+    for row in result:
+        latlon = []
+        latlon.append(row[0])
+        latlon.append(row[1])
+        arena_dict = {
+            "location": latlon,
+            "arenaName": row[2],
+            "teamName": row[3],
+            "arenaURL": row[4],
+            "sponsorURL": row[5]
+        }
+        arenaInfo.append(arena_dict)
+
+    print(arenaInfo)
+
     session.close()
 
-    return render_template("index.html")
+    return render_template("index.html", arenaInfo=arenaInfo)
 
 
 @app.route("/salaries")
@@ -54,7 +71,7 @@ def salaries():
 
     # Query for distinct divisions and their parent conferences
     results = session.query(Win_Counts.division, Win_Counts.conference)\
-        .distinct().order_by(Win_Counts.division.desc())
+        .distinct()
 
     for row in results:
         # append distinct divisions to labels
@@ -70,7 +87,7 @@ def salaries():
 
     # Query distinct teams
     team_results = session.query(Win_Counts.team, Win_Counts.division)\
-        .distinct().order_by(Win_Counts.team.desc())
+        .distinct()
 
     for row in team_results:
         # append distinct teams to labels
@@ -107,19 +124,27 @@ def salaries():
         values=values
     )
 
+
 @app.route("/wins")
 def wins():
     # Initialize database session
     session1 = Session(engine)
 
-    #For Team Win_Counts
+    # For Team Win_Counts
     # Base labels and parents
     label = ["League", "Western Conference", "Eastern Conference"]
     parent = ["", "League", "League"]
-    value = [""]
+    value = []
+
+    # Get league total wins
+    results = session1.query(func.sum(Win_Counts.win_count))
+    value.append(results[0][0])
 
     # Query for distinct divisions and their parent conferences
-    results = session1.query(Win_Counts.division, Win_Counts.conference).distinct().order_by(Win_Counts.division.desc())
+    results = session1.query(
+        Win_Counts.division,
+        Win_Counts.conference
+    ).distinct().order_by(Win_Counts.division.desc())
 
     for row in results:
         # append distinct divisions to labels
@@ -137,23 +162,29 @@ def wins():
         # append their parent divisions to parents
         parent.append(row[1])
 
-    win_totals1 = session1.query(func.sum(Win_Counts.win_count), Win_Counts.conference).group_by(Win_Counts.conference).order_by(Win_Counts.conference.desc())
+    win_totals1 = session1.query(
+        func.sum(Win_Counts.win_count),
+        Win_Counts.conference
+    ).group_by(Win_Counts.conference).order_by(Win_Counts.conference.desc())
     
     for row in win_totals1:
         value.append(row[0])
 
-    win_totals2 = session1.query(func.sum(Win_Counts.win_count), Win_Counts.division).group_by(Win_Counts.division).order_by(Win_Counts.division.desc())
+    win_totals2 = session1.query(
+        func.sum(Win_Counts.win_count),
+        Win_Counts.division
+    ).group_by(Win_Counts.division).order_by(Win_Counts.division.desc())
     
     for row in win_totals2:
         value.append(row[0])
 
-    win_totals3 = session1.query(func.sum(Win_Counts.win_count), Win_Counts.team).group_by(Win_Counts.team).order_by(Win_Counts.team.desc())
+    win_totals3 = session1.query(
+        func.sum(Win_Counts.win_count),
+        Win_Counts.team
+    ).group_by(Win_Counts.team).order_by(Win_Counts.team.desc())
     
     for row in win_totals3:
         value.append(row[0])
-
-    #print(value)
-
 
     # Close connection
     session1.close()
