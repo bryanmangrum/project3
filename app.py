bc67a766b19063f9860d58a99137eee62864bb2b
charlesphil <71610407+charlesphil@users.noreply.github.com>
2022-04-16 07:51:14 -0400

    Removed unneeded import

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import numpy as np
-import random
 
 # Initial connection to database
 
