7c6352fa582c85f8fcf8875b619a67128acb5e55
coye-roseberry <coye.a.roseberry@gmail.com>
2022-04-15 22:02:05 -0400

    Homepage Map
    Updated script to on homepage to show/set markers and set popup text

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import config
+  
 
 # Initial connection to database
-engine = create_engine(f"postgresql://postgres:password@localhost:5432/basketball")
+engine = create_engine(f"postgresql://postgres:{config.pw}@localhost:5432/{config.db}")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -24,7 +26,7 @@ def index():
     session = Session(engine)
     arenaInfo = []
     result = session.query(Arenas.latitude, Arenas.longitude, Arenas.arena,
-                           Arenas.team, Arenas.arenaurl, Arenas.sponsorurl)
+                           Arenas.team,Arenas.sponsor, Arenas.arenaurl, Arenas.sponsorurl, Arenas.imgurl)
     for row in result:
         latlon = []
         latlon.append(row[0])
@@ -33,8 +35,10 @@ def index():
             "location": latlon,
             "arenaName": row[2],
             "teamName": row[3],
-            "arenaURL": row[4],
-            "sponsorURL": row[5]
+            "sponsorName": row[4],
+            "arenaURL": row[5],
+            "sponsorURL": row[6],            
+            "logoURL": row[7]
         }
         arenaInfo.append(arena_dict)
 
